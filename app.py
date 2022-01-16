e00ca552c2f71b1839b63b540a00ca6e0c0dc361
Salik Hussaini <47673489+salikhussaini@users.noreply.github.com>
2022-01-16 13:14:10 -0600

    Updates

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
+import pandas as pd
 import numpy as np
+
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go # Plotting
 import plotly.express as px
 import plotly.graph_objs as go
